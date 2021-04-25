0ac4bccb06f11a882f10424bc971910d7b6522e3
Haamed Sultani <haamed@fokoretail.com>
2021-04-25 11:10:19 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Haamed Sultani on 2021-04-25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
